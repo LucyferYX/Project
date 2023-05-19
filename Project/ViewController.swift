2bc331564083efccdca3ac8c1a09a4915f272614
Lucyfer <liene.krista@gmail.com>
2023-05-19 23:43:33 +0300

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by liene.krista.neimane on 19/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
