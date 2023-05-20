66609ac948fe155e5ec7b366ccc5bafb7cabe35f
Lucyfer <liene.krista@gmail.com>
2023-05-21 02:13:56 +0300

    More work, issues resolved

--- Project/Controllers/MainViewController.swift
+++ Project/Controllers/MainViewController.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var ThemeSwitch: UISwitch!
+    @IBOutlet weak var SettingsButton: UIButton!
+    @IBOutlet weak var InfoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
-    @IBAction func InfoButton(_ sender: UIButton) {
-        
+    
+    @IBAction func InfoButtonTap(_ sender: UIButton) {
     }
+    
+    @IBAction func SettingsButtonTap(_ sender: UIButton) {
+    }
+    
     
     // Background image with rounded bottom corners
     @IBOutlet weak var BG: UIImageView!
@@ -31,5 +39,22 @@ class MainViewController: UIViewController {
         shape.path = maskPath.cgPath
         BG.layer.mask = shape
     }
+    
+    
+    // Light and dark theme slider
+    @IBAction func ThemeSlider(_ sender: UISwitch) {
+        if sender.isOn {
+                // Dark theme
+                BG.backgroundColor = UIColor.black
+                InfoButton.setTitleColor(UIColor.white, for: .normal)
+                SettingsButton.setTitleColor(UIColor.white, for: .normal)
+            } else {
+                // Light theme
+                BG.backgroundColor = UIColor.white
+                InfoButton.setTitleColor(UIColor.black, for: .normal)
+                SettingsButton.setTitleColor(UIColor.black, for: .normal)
+            }
+    }
+    
 }
 
