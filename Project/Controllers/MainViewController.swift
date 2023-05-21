6f06fbfa6ad0676e4c2d28cd69f6f1b45f02a833
Lucyfer <liene.krista@gmail.com>
2023-05-21 20:02:29 +0300

    Themes

--- Project/Controllers/MainViewController.swift
+++ Project/Controllers/MainViewController.swift
@@ -13,14 +13,22 @@ class MainViewController: UIViewController {
     @IBOutlet weak var SettingsButton: UIButton!
     @IBOutlet weak var InfoButton: UIButton!
     
+    var darkIsOn: Bool = false
+    
+    // Custom colours
+    let customGreen = UIColor(red: 36/255, green: 79/255, blue: 37/255, alpha: 1)
+    let customPink = UIColor(red: 0.85, green: 0.61, blue: 0.86, alpha: 1)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        darkThemeIsOn(isOn: ThemeSwitch.isOn)
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func InfoButtonTap(_ sender: UIButton) {
+        basicActionAlert(title: "App information", message: "Creator: Liene Krista Neimane")
     }
     
     @IBAction func SettingsButtonTap(_ sender: UIButton) {
@@ -31,7 +39,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var BG: UIImageView!
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        
         let maskPath = UIBezierPath(roundedRect: BG.bounds,
                                     byRoundingCorners: [.bottomLeft, .bottomRight],
                                     cornerRadii: CGSize(width: 70, height: 30))
@@ -43,18 +51,36 @@ class MainViewController: UIViewController {
     
     // Light and dark theme slider
     @IBAction func ThemeSlider(_ sender: UISwitch) {
-        if sender.isOn {
-                // Dark theme
-                BG.backgroundColor = UIColor.black
-                InfoButton.setTitleColor(UIColor.white, for: .normal)
-                SettingsButton.setTitleColor(UIColor.white, for: .normal)
-            } else {
-                // Light theme
-                BG.backgroundColor = UIColor.white
-                InfoButton.setTitleColor(UIColor.black, for: .normal)
-                SettingsButton.setTitleColor(UIColor.black, for: .normal)
-            }
+        darkThemeIsOn(isOn: sender.isOn)
     }
     
+    func darkThemeIsOn(isOn: Bool){
+        if isOn {
+            // Dark theme
+            BG.backgroundColor = UIColor.black
+//            InfoButton.setTitleColor(UIColor.white, for: .normal)
+//            SettingsButton.setTitleColor(UIColor.white, for: .normal)
+            SettingsButton.tintColor = customGreen
+            InfoButton.tintColor = customGreen
+            overrideUserInterfaceStyle = .dark
+        } else {
+            // Light theme
+            BG.backgroundColor = UIColor.white
+//            InfoButton.setTitleColor(UIColor.black, for: .normal)
+//            SettingsButton.setTitleColor(UIColor.black, for: .normal)
+            SettingsButton.tintColor = customPink
+            InfoButton.tintColor = customPink
+            overrideUserInterfaceStyle = .light
+        }
+    }
+    
+    
+    // Info button alert
+    func basicActionAlert(title: String?, message: String?){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
 }
-
